0866a1058cd87889d80ad8d28fc8942ea2196f55
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2022-06-20 20:38:23 +0800

    hotfix

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
-	build: {
-		outDir: "./docs",
-	},
+  plugins: [svelte()],
+  build: {
+    outDir: "./docs",
+  },
+  base: "./",
 });
